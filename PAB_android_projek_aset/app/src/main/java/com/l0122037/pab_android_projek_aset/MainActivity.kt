294b2ed7d26e0777284d38df715639af5f8954c8
Abe <abraham120604@gmail.com>
2024-06-13 00:54:23 +0700

    update-1
    added navigation to blueprint

--- PAB_android_projek_aset/app/src/main/java/com/l0122037/pab_android_projek_aset/MainActivity.kt
+++ PAB_android_projek_aset/app/src/main/java/com/l0122037/pab_android_projek_aset/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.l0122037.pab_android_projek_aset.databinding.ActivityMainBinding
+import com.l0122037.pab_android_projek_aset.ui.bluprint.BluprintFragment // Pastikan impor ini ada
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -34,15 +34,19 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+            setOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow),
+            drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Menambahkan BluprintFragment jika savedInstanceState null
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, BluprintFragment.newInstance("param1", "param2"))
+                .commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
