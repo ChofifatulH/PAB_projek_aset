679bfb5e37cdae15a046e65f72602aa1b6da25d6
ChofifatulH <chofifatul23@gmail.com>
2024-06-12 16:51:50 +0700

    main_kedua

---
+++ PAB_android_projek_aset/app/src/main/java/com/l0122037/pab_android_projek_aset/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.l0122037.pab_android_projek_aset.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
